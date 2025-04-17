1041b57104b237a12f46aaed35f11b48de0f5bf8
Refael <raphael6163@gmail.com>
2025-04-17 23:33:39 +0300

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
       <div 
         className="absolute inset-0 bg-cover bg-center bg-no-repeat"
         style={{
-          backgroundImage: `url('/lovable-uploads/me.jpg')`,
+          backgroundImage: `url('/lovable-uploads/CAPITAL.png')`,
           width: '100%',
           height: '100%'
         }}
